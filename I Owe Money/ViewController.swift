932e8eb9a0196c4bb27b59ec6cb926e8aa6ec93a
Kevin Wang <kvin.w2@gmail.com>
2017-05-21 16:48:18 -0400

    Initial Commit

---
+++ I Owe Money/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Owe Money
+//
+//  Created by Kevin on 2017-05-21.
+//  Copyright © 2017 Kevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
